b125dd64d8309345e1a13a7814e053fdfe09bf6f
anatalibo <antonio.natali@unibo.it>
2019-02-26 10:25:46 +0100

    Lab0

---
+++ issStart2019/README.md
@@ -0,0 +1,2 @@
+# issStart2019
+Start Lab for Course 72939 : Software Systems Engineering M - Antonio Natali - DISI - University of Bologna
---
+++ issStart2019/src/main/java/issStart2019/AppJava.java
@@ -0,0 +1,11 @@
+package issStart2019;
+
+public class AppJava {
+    public String getGreeting() {
+        return "Hello world FROM JAVA.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new AppJava().getGreeting());
+    }
+}
