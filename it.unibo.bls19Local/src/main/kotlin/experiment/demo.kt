67faf1f262b083c044e93607f0d5eded122e1325
anatalibo <antonio.natali@unibo.it>
2019-03-13 11:55:40 +0100

    Kotlin intro 1

--- it.unibo.bls19Local/src/main/kotlin/experiment/demo.kt
+++ it.unibo.bls19Local/src/main/kotlin/experiment/demo.kt
@@ -1,10 +1,25 @@
 package experiment
 
+
+
 var counter = 0 //type inferred
 
-fun incCounter() : Unit{ counter++ } //EXPRESSION BLOCK
+fun xxx() : Unit{
+    val s1 = "a"
+    var s2 = "a"
+    println( "s1 === s2 : ${s1 === s2} ") //true
+    println( "s1 == s2  : ${s1 == s2} ") //true
+    s2 = "b"
+    println( "s1 === s2 : ${s1 === s2} ") //false
+    s2 = "a"
+    println( "s1 === s2 : ${s1 === s2} ") //true
+    val a = java.io.File("a")
+    val b = java.io.File("a")
+    println( "a === b : ${a === b}") //false
+    println( "a == b  : ${a == b}") //true
+
+}
 
 fun main(){
-    for( i in 1..5 ) incCounter()
-    println( "counter=$counter")
+    xxx()
 }
