f10618a6c728e85f4862fd3ec46f0a9b5ae006c0
anatalibo <antonio.natali@unibo.it>
2019-03-17 12:09:48 +0100

    Kotlin no js

--- it.unibo.kjs/Readme.txt
+++
@@ -1,2 +0,0 @@
-https://superkotlin.com/kotlin-javascript/
-
--- it.unibo.kjs/src/main/kotlin/Main0.kt
+++
@@ -1,6 +0,0 @@
-import kotlin.browser.*
-import kotlin.dom.*
-
-fun main(args: Array<String>) {
-    document.getElementById("tooltip")?.appendText("The first step")
-}
