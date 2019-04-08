786abf618e13d052a80a9b5c137cf370e9a3b30a
anatalibo <antonio.natali@unibo.it>
2019-04-08 09:16:25 +0200

    prodCons sequence

--- it.unibo.bls19d/src/main/kotlin/it/unibo/qak/prodConsKotlin/prodSequenceKotlin.kt
+++ it.unibo.bls19d/src/main/kotlin/it/unibo/qak/prodConsKotlin/prodSequenceKotlin.kt
@@ -10,28 +10,49 @@ import kotlinx.coroutines.channels.produce
 
 val prodContext = newSingleThreadContext("myThread")
 
+val fiboSeq = sequence{
+    var a = 0
+    var b = 1
+    yield(1)           //first
+    while (true) {
+        yield(a + b)   //next
+        val tmp = a + b
+        a = b
+        b = tmp
+    }
+}
+
 val seqProd  = sequence{
     var v = 1
-    while(true){
-        println( "seqProd produces $v in ${sysUtil.curThread()}")
-        yield( v++ )
+    for(i in 1..3){
+        yield( v  )
+        //println( "seqProd produced $v in ${sysUtil.curThread()}")
+        v++
     }
+    //println( "seqProd generateSequence  ")
+    yieldAll( generateSequence(2) { it * 2  } )
 }
 
 suspend fun seqcons1( scope : CoroutineScope){
     println("seqcons1 STARTS")
     scope.launch {
-        val vlist = seqProd.take(3).toList()
-        println("seqcons1 receives $vlist in ${sysUtil.curThread()}")
-        delay(2000)
+        for( i in 0 .. 5 ) {
+            val v = seqProd.elementAt(i)
+            //val vlist = seqProd.take(3).toList()
+            println("seqcons1 $i receives $v in ${sysUtil.curThread()}")
+            delay(100) //release control
+        }
     }
 }
 
 suspend fun seqcons2( scope : CoroutineScope ){
     println("seqcons2 STARTS")
     scope.launch {
-        val vlist = seqProd.take(7).filter { it % 2 != 0 }.toList()
-        println("seqcons2 receives $vlist in ${sysUtil.curThread()}")
+        for( i in 1 .. 3 ) {
+            val vlist = seqProd.take(i*3).filter { it % 2 == 0 }.toList()
+            println("seqcons2 receives $vlist in ${sysUtil.curThread()}")
+            delay(100) //release control
+        }
     }
 }
 
@@ -39,6 +60,5 @@ suspend fun seqcons2( scope : CoroutineScope ){
 fun main() = runBlocking{
     seqcons1(this)
     seqcons2(this)
-    //delay( 1000)
     println( "BYE")
 }
