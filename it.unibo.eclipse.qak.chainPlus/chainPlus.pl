38b6c949bc16f54fce1011f52ef0f5472d29efc0
anatalibo <antonio.natali@unibo.it>
2019-05-06 22:46:47 +0200

    update

--- it.unibo.eclipse.qak.chainPlus/chainPlus.pl
+++ it.unibo.eclipse.qak.chainPlus/chainPlus.pl
@@ -1,10 +1,7 @@
 %====================================================================================
 % chainPlus description   
 %====================================================================================
-context(ctxbutton, "localhost",  "TCP", "8010" ).
-context(ctxledsplus, "localhost",  "TCP", "8050" ).
-qactor( led1, ctxledsplus, "resources.LedActork").
-qactor( led2, ctxledsplus, "resources.LedActork").
-qactor( button, ctxbutton, "it.unibo.button.Button").
-qactor( control, ctxbutton, "it.unibo.control.Control").
+context(ctxbutton, "192.168.1.17",  "TCP", "8010" ).
+context(ctxledsplus, "192.168.1.18",  "TCP", "8050" ).
 qactor( dynamo, ctxledsplus, "it.unibo.dynamo.Dynamo").
+qactor( ledplus1, ctxledsplus, "resources.LedActork").
--- it.unibo.eclipse.qak.chainPlus/src/it/unibo/ctxButton/MainCtxButton.kt
+++ it.unibo.eclipse.qak.chainPlus/src/it/unibo/ctxButton/MainCtxButton.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
 	QakContext.createContexts(
-	        "localhost", this, "chainplus.pl", "sysRules.pl"
+	        "192.168.1.17", this, "chainplus.pl", "sysRules.pl"
 	)
 }
 
