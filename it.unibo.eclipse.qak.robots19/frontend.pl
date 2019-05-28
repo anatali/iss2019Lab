01afb716bbfbf148b778be8edc0fadd66bf20d71
anatalibo <antonio.natali@unibo.it>
2019-05-28 14:07:52 +0200

    week15

--- it.unibo.eclipse.qak.robots19/frontend.pl
+++ it.unibo.eclipse.qak.robots19/frontend.pl
@@ -3,6 +3,6 @@
 %====================================================================================
 mqttBroker("localhost", "1883").
 context(ctxfrontend, "localhost",  "MQTT", "0" ).
-context(ctxrobotmind, "otherhost",  "MQTT", "0" ).
- qactor( robotmind, ctxrobotmind, "external").
+context(ctxrobotmindnomv, "otherhost",  "MQTT", "0" ).
+ qactor( robotmind, ctxrobotmindnomv, "external").
   qactor( frontend, ctxfrontend, "it.unibo.frontend.Frontend").
--- it.unibo.eclipse.qak.robots19/src/it/unibo/ctxRobotMindNoMv/MainCtxRobotMindNoMv.kt
+++ it.unibo.eclipse.qak.robots19/src/it/unibo/ctxRobotMindNoMv/MainCtxRobotMindNoMv.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
 	QakContext.createContexts(
-	        "localhost", this, "robotmindnomvc.pl", "sysRules.pl"
+	        "otherhost", this, "frontend.pl", "sysRules.pl"
 	)
 }
 
