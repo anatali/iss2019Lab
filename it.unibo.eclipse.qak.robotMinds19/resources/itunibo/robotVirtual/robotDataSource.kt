281553e034ced3e042cdfb1bfbce69f8a5a0cc7d
Antonio Natali <antonio.natali@unibo.it>
2019-07-15 10:51:55 +0200

    vitual robot robotDataSource

--- it.unibo.eclipse.qak.robotMinds19/resources/itunibo/robotVirtual/robotDataSource.kt
+++ it.unibo.eclipse.qak.robotMinds19/resources/itunibo/robotVirtual/robotDataSource.kt
@@ -8,9 +8,14 @@ import it.unibo.kactor.ActorBasic
 import it.unibo.kactor.MsgUtil
 import kotlinx.coroutines.launch
 import it.unibo.kactor.ActorBasicFsm
+import kotlinx.coroutines.delay
 
 class robotDataSource(name : String, val owner : ActorBasicFsm , val filter : ActorBasic?,
 					  val inFromServer: BufferedReader?) : ApplActorDataStream(name,owner.scope){
+	
+var obstacleName = ""
+val event = MsgUtil.buildEvent(name,"sonarRobot","sonar( 5 )")
+	
 	init{
 		scope.launch{  autoMsg("start","start(1)") }
 		if( filter != null ) subscribe(filter)
@@ -19,28 +24,35 @@ class robotDataSource(name : String, val owner : ActorBasicFsm , val filter : Ac
 	override suspend fun elabData(data : String ){
                 while (true) {
                     try {
-                      //println("robotDataSource | READLINE")
-                       val inpuStr = inFromServer?.readLine()
-                        val jsonMsgStr =
+                        val inpuStr = inFromServer?.readLine()
+                       //println( "robotDataSource | inpuStr= $inpuStr")
+                       val jsonMsgStr =
                             inpuStr!!.split(";".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[1] 
                          val jsonObject = JSONObject(jsonMsgStr)
-                        println( "robotDataSource | inpuStr= $jsonMsgStr type: " + jsonObject.getString("type"));
-                        when (jsonObject.getString("type")) {
-                            "webpage-ready" -> println("webpage-ready ")
-                            "sonar-activated" -> {
-                                //println("sonar-activated ")
-                                val jsonArg   = jsonObject.getJSONObject("arg")
-                                val sonarName = jsonArg.getString("sonarName")
-                                val distance  = jsonArg.getInt("distance")
-  								val m1 = "sonar($sonarName, $distance)"
-								//println( "robotDataSource EMIT $m1"   );
- 								emitLocalStreamEvent(m1) 
-                           }
-                            "collision" -> { 
-  								val event = MsgUtil.buildEvent(name,"sonarRobot","sonar( 5 )")
-								emitLocalStreamEvent(event) 
-                           }
-                        }
+						val arg = jsonObject.get("arg").toString()
+						//println( "robotDataSource | arg= ${arg}  ${arg.equals(obstacleName) }"   )
+						if( arg.equals(obstacleName) ) {
+							emitLocalStreamEvent(event)
+						}
+						else{
+	                        when (jsonObject.getString("type")) {
+	                            "webpage-ready" -> println("webpage-ready ")
+	                            "sonar-activated" -> {
+	                                //println("sonar-activated ")
+	                                val jsonArg   = jsonObject.getJSONObject("arg")
+	                                val sonarName = jsonArg.getString("sonarName")
+	                                val distance  = jsonArg.getInt("distance")
+	  								val m1 = "sonar($sonarName, $distance)"
+									//println( "robotDataSource EMIT $m1"   );
+	 								emitLocalStreamEvent(m1) 
+	                           }
+	                            "collision" -> {
+									obstacleName = jsonObject.get("arg").toString()
+									emitLocalStreamEvent(event)
+	                           }				    
+	                        }
+						}
+						
                     } catch (e: Exception) {
                         //e.printStackTrace()
 						println("robotDataSource | ERROR $e   ")
